1f03f6b3a6c9539d0d1102b2ea86512e000219e0
SymphonyX <pete1064@hotmail.com>
2016-07-22 14:16:48 -0400

    W updates with projected data, M updates with original data.

--- MixtureOfSubspaces.py
+++ MixtureOfSubspaces.py
@@ -9,14 +9,14 @@ def softmax(x):
 
 class MixtureOfSubspaces:
 
-    def __init__(self, num_subspaces, num_dimensions):
+    def __init__(self, num_subspaces, proj_dimension, original_dimensions):
         '''
         subspaces: list of subspaces NxM, N samples, M dimensions
         '''
         #W experts weight matrix
         self.W = np.random.random( (num_subspaces, num_dimensions) )
         #g combination weight for predictors
-        self.M = np.random.random( (num_subspaces, num_dimensions) )
+        self.M = np.random.random( (num_subspaces, original_dimensions) )
 
 
     def _gating_weights(self, X, num_subspaces):
@@ -27,30 +27,29 @@ class MixtureOfSubspaces:
             g[:,k] = ge
         return g
 
-    def _make_experts_prediction(self, X, subspaces):
-        predictions = np.zeros( (len(subspaces), X.shape[0]))
-        for i, S in enumerate(subspaces):
-            X_proj = X.dot(S.dot(S.T))
-            prediction = self.W[i].dot(X_proj.T)
+    def _make_experts_prediction(self, X, X_proj):
+        predictions = np.zeros( (len(X_proj), X.shape[0]))
+        for i, x_proj in enumerate(X_proj):
+            prediction = self.W[i].dot(x_proj.T)
             predictions[i] = prediction
 
         return predictions
 
-    def make_prediction(self, X, subspaces):        
-        predictions = self._make_experts_prediction(X, subspaces)
-        g = self._gating_weights(X, len(subspaces))
+    def make_prediction(self, X, X_proj):        
+        predictions = self._make_experts_prediction(X, X_proj)
+        g = self._gating_weights(X, len(X_proj))
         mixture_prediction = np.sum(g * predictions, axis=0)
         return mixture_prediction
 
 
-    def _line_search(self, current_error, grad_w, grad_m, X, Y, subspaces):
+    def _line_search(self, current_error, grad_w, grad_m, X, Y, X_proj):
         alpha = 1.0
         W_cpy = self.W.copy()
         M_cpy = self.M.copy()
         for i in range(5):
             self.W += alpha * grad_w
             self.M += alpha * grad_m
-            new_error = np.mean( (Y - self.make_prediction(X, subspaces))**2, axis=0)
+            new_error = np.mean( (Y - self.make_prediction(X, X_proj))**2, axis=0)
             self.W = W_cpy.copy()
             self.M = M_cpy.copy()
             if new_error < current_error:
@@ -58,12 +57,12 @@ class MixtureOfSubspaces:
             alpha /= 10
         return alpha
 
-    def train_mixture(self, X, Y, subspaces):
+    def train_mixture(self, X, Y, X_proj):
         
         current_error = float("inf")
         while True:
-            predictions = self._make_experts_prediction(X, subspaces)
-            g = self._gating_weights(X, len(subspaces))
+            predictions = self._make_experts_prediction(X, X_proj)
+            g = self._gating_weights(X, len(X_proj))
             mixture_prediction = np.sum(g * predictions, axis=0)
 
             MSE = np.mean( (Y - mixture_prediction)**2, axis=0)
@@ -76,10 +75,10 @@ class MixtureOfSubspaces:
                 weighted_error = (errors * g[k]).reshape((g[k].shape[0], 1))
                 expert_error = (predictions[k] - mixture_prediction).reshape( (predictions[k].shape[0], 1) )
 
-                grad_w[k] = np.mean(weighted_error * X, axis=0)
+                grad_w[k] = np.mean(weighted_error * X_proj, axis=0)
                 grad_m[k] = np.mean(weighted_error * expert_error * X, axis=0)
 
-            alpha = self._line_search(MSE, grad_w, grad_m, X, Y, subspaces)
+            alpha = self._line_search(MSE, grad_w, grad_m, X, Y, X_proj)
             self.W += alpha * grad_w
             self.M += alpha * grad_m
 
